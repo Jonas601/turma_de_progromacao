6e1861330f6110288e3835f6d1f7b23b05a22a0e
Jonas Soares <jonastrabalho31@gmail.com>
2025-06-12 11:55:42 -0400

    Create testedecodigos.js

---
+++ operadores_logicos/testedecodigos.js
@@ -0,0 +1,8 @@
+
+var nome = " Jonas"
+const saudacao = "bem-vindo" 
+console.log (saudacao +""+  nome);
+
+var nomespokemon = ["pikachu","chamander","bulbasauro"]
+var timepokemon = [["pikachu" , "m", 1] ,["chamander" , "f" ,3]]
+console.log("o pokemon" + timepokemon [0][1])
