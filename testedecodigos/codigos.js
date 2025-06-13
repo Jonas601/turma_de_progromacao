ff962c8ea6d725bbdd8ca3e545429ad3d05ebc1c
Jonas Soares <jonastrabalho31@gmail.com>
2025-06-13 10:34:11 -0400

    Update codigos.js

--- testedecodigos/codigos.js
+++ testedecodigos/codigos.js
@@ -6,3 +6,8 @@ console.log (saudacao +""+  nome);
 var nomespokemon = ["pikachu","chamander","bulbasauro"]
 var timepokemon = [["pikachu" , "m", 1] ,["chamander" , "f" ,3]]
 console.log("o pokemon" + timepokemon [0][1])
+
+for(let contador = 1;contador<=5;contador++)
+{
+  (console.log(contador))
+}
