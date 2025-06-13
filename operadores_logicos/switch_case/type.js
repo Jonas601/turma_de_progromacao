5985705d1d46d0e1f01bbdd86316ac238713a8e7
Jonas Soares <jonastrabalho31@gmail.com>
2025-06-13 11:44:48 -0400

    Create type.js

---
+++ operadores_logicos/switch_case/type.js
@@ -0,0 +1,34 @@
+let dia = "quinta";
+switch (dia) {
+case "segunda":
+console.log ("dia de começa a semana");
+break ;
+}
+
+switch (dia) {
+case "terça":
+console.log ("hora de aprender coisas novas");
+break ; 
+}
+
+switch (dia) {
+case "quarta":
+console.log ("aguente firme ,metade da semana");
+break ; 
+}
+
+switch (dia) {
+case "quinta":
+console.log ("quase lá, mais um pouco de esforço");
+break ; 
+}
+
+switch (dia) {
+case "sexta":
+console.log ("dia de comemora, o fim de semana");
+break ; 
+
+default:
+    console.log("final de semana chegou");
+    }
+
